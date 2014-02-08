eb0c474af7c3606f711bf7bb6eced4e532651789
Simone Dalla <simodalla@gmail.com>
2014-02-08 13:41:27 +0100

    initial project

---
+++ demo_mezzanine_3/demo_app/tests_aaa.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class AAATest(TestCase):
+    def test_3(self):
+        self.assertEqual(1, 1)
+
+    def test_4(self):
+        self.assertEqual(1, 1)
